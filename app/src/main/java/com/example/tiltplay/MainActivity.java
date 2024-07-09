4fb548ccbac65572b31d177f5522e7f15898c5f8
ARYA <rohankumar7522@gmail.com>
2024-07-09 11:50:36 +0530

    vbfgb

---
+++ app/src/main/java/com/example/tiltplay/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.tiltplay;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    MediaPlayer mp;
+    SensorManager sm;
+    Sensor s;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        mp=MediaPlayer.create(this,R.raw.ab);
+        sm=(SensorManager) getSystemService(SENSOR_SERVICE);
+        s=sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this,s,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x=event.values[0];
+        float y=event.values[1];
+        float z=event.values[2];
+        int y1=(int)y;
+        int z1=(int)z;
+        int x1=(int)x;
+        if (x1!=0)
+        {
+            mp.start();
+        }
+        else
+        {
+            mp.pause();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
